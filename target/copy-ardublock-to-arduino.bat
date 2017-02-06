b2a98749efd65367f5e039107982f77dcb5c984d
Picuino <picuino@picuino.com>
2017-02-06 18:52:38 +0100

    Fix folder Duinoedu uppercase error

---
+++ target/copy-ardublock-to-arduino.bat
@@ -0,0 +1,4 @@
+@echo off
+set USERPROFILE=D:\Datos\User
+copy ardublock-all.jar %USERPROFILE%\Arduino\tools\ArduBlockTool\tool\ardublock-all.jar
+pause
--- target/make-jar.bat
+++ target/make-jar.bat
@@ -2,8 +2,8 @@
 
 set java="C:\Program Files\Java\jdk1.8.0_111\bin\jar.exe"
 set source1="classes"
-set source2="../../openblocks/target/classes"
+set source2="../../picuino-openblocks/target/classes"
 
-%java% cfe ardublock-all.jar com.ardublock.Main -C %source1% . -C %source2% .
+%java% cfm ardublock-all.jar Manifest.txt -C %source1% . -C %source2% .
 
 pause
