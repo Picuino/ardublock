eb5065de0bb8099ef8732b9f3b96bd829c813ad5
Picuino <picuino@picuino.com>
2017-02-08 23:20:25 +0100

    Delete comments in ardublock.properties and translations

---
+++ target/_delete_properties_names.py
@@ -0,0 +1,37 @@
+import os
+import re
+
+input_path = 'classes/com/ardublock/block'
+input_file = 'ardublock([_a-zA-Z]{3,6})?\.properties'
+
+output_path = 'new_properties'
+output_names = 'properties_names.xml'
+
+name_pattern = 'b[cdg]\.[0-9a-zA-Z_-]*'
+
+def main():
+  names = open(os.path.join(output_path, output_names), 'rt').read()
+  names = names.split('\n')
+  
+  files = [f for f in os.listdir(input_path) if re.match(input_file, f)]
+  for filename in files:
+      process(input_path, filename, names)
+
+def process(input_path, filename, xml_names):
+    print filename
+    all_filename = os.path.join(input_path, filename)
+ 
+    fi = open(all_filename, 'rt')
+    code = []
+    for li in fi:
+      name = re.findall(name_pattern, li)
+      if name and not (name[0] in xml_names):
+        continue
+      code.append(li)
+
+    fi.close()
+    fo = open(os.path.join(output_path, filename), 'wt')
+    fo.write(''.join(code))
+    fo.close()
+
+main()
---
+++ target/_extract_property_names.py
@@ -0,0 +1,28 @@
+import os
+import re
+
+input_path = 'classes/com/ardublock/block'
+input_file = 'ardublock.properties'
+
+output_path = 'new_properties'
+output_file = 'properties_names.xml'
+
+name_pattern = 'b[cdg]\.[0-9a-zA-Z_-]*'
+
+def main():
+  # Extract names 
+  data = open(os.path.join(input_path, input_file), 'rt').read()
+  all_names = re.findall(name_pattern, data)
+  names = []
+  for name in all_names:
+    if not name in names:
+      names.append(name)
+
+  # Write sorted list of names to disk
+  names.sort()
+  for name in names: print name
+  fo = open(os.path.join(output_path, output_file), 'wt')
+  fo.write('\n'.join(names))
+  fo.close()
+
+main()
